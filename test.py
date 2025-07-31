03821f2b7c6ab3e5db46bc2d2111f732b07c2af2
Milan Vrábeľ <milan.vrabel@seznam.cz>
2025-07-31 10:52:03 +0200

    Přidán requests a requirements

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("Hallo Python")
+def pozdrav(jmeno):
+    return f"Ahoj, {jmeno}!"
+
+if __name__ == "__main__":
+    j = input("Zadej jméno: ")
+    print(pozdrav(j))
+import requests
+print("Requests verze:", requests.__version__)
